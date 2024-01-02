683dbe07450800bf0fde5ae63f50c89226698397
MRRollMan <konkow@ukr.net>
2024-01-03 00:54:34 +0200

    Base formatting and file encoding for logger

--- src/logger.py
+++ src/logger.py
@@ -5,17 +5,25 @@ from logging.handlers import TimedRotatingFileHandler
 from datetime import datetime
 
 # Створюємо папку logs якщо її немає
-if not os.path.exists('src/logs'):
-    os.makedirs('src/logs')
+if not os.path.exists("src/logs"):
+    os.makedirs("src/logs")
 
 # Логування у файл і на консоль
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-log_file_name = 'src/logs/log'
-file_handler = TimedRotatingFileHandler(log_file_name, when="midnight", interval=1, backupCount=30)
+log_file_name = "src/logs/log.log"
+file_handler = TimedRotatingFileHandler(
+    log_file_name, when="midnight", interval=1, backupCount=30, encoding="utf-8"
+)
 file_handler.suffix = "%d-%m-%Y.log"
-file_handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] - %(message)s', datefmt='%d.%m.%Y, %H:%M:%S'))
+file_handler.setFormatter(
+    logging.Formatter(
+        "%(asctime)s [%(levelname)s] - %(message)s", datefmt="%d.%m.%Y, %H:%M:%S"
+    )
+)
 logger.addHandler(file_handler)
 console_handler = logging.StreamHandler()
-console_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s', datefmt='%d.%m.%Y, %H:%M:%S'))
+console_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(message)s", datefmt="%d.%m.%Y, %H:%M:%S")
+)
 logger.addHandler(console_handler)
