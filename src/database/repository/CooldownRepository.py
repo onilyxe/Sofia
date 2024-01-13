03f2f93cdf195361bbd225caf9cb555dd81f7d82
MRRollMan <konkow@ukr.net>
2024-01-13 03:39:19 +0200

    Change naming for cooldown type in Repository

--- src/database/repository/CooldownRepository.py
+++ src/database/repository/CooldownRepository.py
@@ -7,10 +7,10 @@ class CooldownRepository:
     def __init__(self, connection: Connection):
         self.connection = connection
 
-    async def get_user_cooldown(self, chat_id: int, user_id: int, cooldown: str | Games | None = None) -> list | None:
-        if not cooldown:
+    async def get_user_cooldown(self, chat_id: int, user_id: int, cooldown_type: str | Games | None = None) -> list | None:
+        if not cooldown_type:
             cooldown = "*"
-        data = await self.connection.execute(f"SELECT cooldowns.{cooldown} FROM cooldowns, chat_users AS cu "
+        data = await self.connection.execute(f"SELECT cooldowns.{cooldown_type} FROM cooldowns, chat_users AS cu "
                                              f"INNER JOIN cooldowns c on cu.id = c.chat_user "
                                              f"WHERE cu.chat_id = ? AND cu.user_id = ?",
                                              (chat_id, user_id,))
@@ -22,7 +22,7 @@ class CooldownRepository:
                                              (chat_id, user_id))
         await self.connection.commit()
 
-    async def update_user_cooldown(self, chat_id: int, user_id: int, cooldown: str, new_cooldown: int) -> None:
-        await self.connection.execute(f"UPDATE cooldowns SET {cooldown} = ? WHERE chat_user = "
+    async def update_user_cooldown(self, chat_id: int, user_id: int, cooldown_type: str, new_cooldown: int) -> None:
+        await self.connection.execute(f"UPDATE cooldowns SET {cooldown_type} = ? WHERE chat_user = "
                                       f"(SELECT id FROM chat_users WHERE chat_id = ? AND user_id = ?)",
                                       (new_cooldown, chat_id, user_id))
